4159bc887a38d5eaad56c4b6f7242060503e5879
Huanghaojie <136454929@qq.com>
2021-10-12 18:03:19 +0800

    commit

---
+++ lib/FunctionWidgetDemo.dart
@@ -0,0 +1,78 @@
+import 'dart:html';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ShareDataWidget extends InheritedWidget {
+
+  final int data; //需要在子树中共享的数据，保存点击次数
+
+  ShareDataWidget({Key?key, required this.data, required Widget child})
+      :super(key: key, child: child);
+
+
+  //该回调决定当data发生变化时，是否通知子树中依赖data的Widget
+  @override
+  bool updateShouldNotify(ShareDataWidget old) {
+    return old.data != data;
+  }
+
+  //定义一个便捷方法，方便子树中的widget获取共享数据
+  static ShareDataWidget? of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<ShareDataWidget>();
+  }
+}
+
+class _ReceiveWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _ReceiveWidgetState();
+}
+
+class _ReceiveWidgetState extends State<_ReceiveWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Text(ShareDataWidget.of(context)!.data.toString());
+  }
+
+  @override
+  void didChangeDependencies() {
+    // TODO: implement didChangeDependencies
+    super.didChangeDependencies();
+    //父或祖先widget中的InheritedWidget改变(updateShouldNotify返回true)时会被调用。
+    //如果build中没有依赖InheritedWidget，则此回调不会被调用。
+    print("Dependencies change");
+  }
+
+}
+
+class InheritedWidgetTestRoute extends StatefulWidget {
+  @override
+  _InheritedWidgetTestRouteState createState() => _InheritedWidgetTestRouteState();
+}
+
+class _InheritedWidgetTestRouteState extends State<InheritedWidgetTestRoute> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return  Center(
+      child: ShareDataWidget( //使用ShareDataWidget
+        data: count,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(bottom: 20.0),
+              child: _ReceiveWidget(),//子widget中依赖ShareDataWidget
+            ),
+            FloatingActionButton(
+              child: Text("Increment"),
+              //每点击一次，将count自增，然后重新build,ShareDataWidget的data将被更新
+              onPressed: () => setState(() => ++count),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
